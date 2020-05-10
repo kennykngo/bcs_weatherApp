087e0a9b2b1628b874531d2b01956126aa215158
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-05-10 00:35:11 -0700

    added weather icon and temp

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 $(document).ready(function () {
-  console.log("hello world");
   var api_key = "6dbe43cb883ce8ea55cc9545b5f2cea3";
-  // var textInput = $("#textInput").val();
+  var textInput = "";
 
-  $.ajax({
-    type: "GET",
-    // url: `api.openweathermap.org/data/2.5/weather?q=San%20Francisco&appid=${api_key}`,
-    url: `https://api.openweathermap.org/data/2.5/weather?q=san%20francisco&appid=${api_key}`,
-    // lat=35&lon=139
-    dataType: "json",
-    success: function (res) {
-      console.log(res);
-      console.log(res.name);
-    },
-    // }).then(function (res) {
+  $("#btnSubmit").on("click", function (e) {
+    e.preventDefault();
+    textInput = $("#textInput").val();
+
+    $.ajax({
+      type: "GET",
+      // url: `api.openweathermap.org/data/2.5/weather?q=San%20Francisco&appid=${api_key}`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${api_key}`,
+      // lat=35&lon=139
+      dataType: "json",
+      success: function (res) {
+        console.log(res);
+        console.log(res.name);
+        console.log(res.main.temp);
+
+        var kelvinTemp = parseFloat(res.main.temp);
+        // console.log(kelvinTemp);
+        console.log((kelvinTemp - 273.15) * 1.8 + 32);
+
+        console.log(res.main.humidity);
+        var weatherIcon = res.weather[0].icon;
+        var iconUrl =
+          "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
+        $("body").prepend(
+          // `<img src="https://openweathermap.org/"+'${weatherIcon}'/>`
+          `<img id="weatherIcon" src="${iconUrl}"/>`
+        );
+      },
+      // }).then(function (res) {
+    });
   });
 
   // (function (res) {
@@ -22,8 +40,3 @@ $(document).ready(function () {
 
   // });
 });
-
-/*
-f('datahere', '201', ')
-
-*/
