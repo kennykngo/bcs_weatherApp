5de747fea9922fb29fe6bb120608b57b79941b07
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-05-26 14:02:13 -0700

    added currentWeather + updated temp

--- app.js
+++ app.js
@@ -1,42 +1,87 @@
 $(document).ready(function () {
-  var api_key = "6dbe43cb883ce8ea55cc9545b5f2cea3";
-  var textInput = "";
+  var api_key_kenny = "6dbe43cb883ce8ea55cc9545b5f2cea3";
+  var date = moment().format("L");
 
   $("#btnSubmit").on("click", function (e) {
+    // $("#mainContainer").removeClass("d-none");
+    var cityName = $("#textInput").val();
+    $("#textInput").val("");
     e.preventDefault();
-    textInput = $("#textInput").val();
+    console.log(textInput);
+    currentWeather(cityName);
+    // searchHistory();
+  });
 
+  function currentWeather(cityName) {
     $.ajax({
       type: "GET",
       // url: `api.openweathermap.org/data/2.5/weather?q=San%20Francisco&appid=${api_key}`,
-      url: `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${api_key}`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key_kenny}`,
       // lat=35&lon=139
       dataType: "json",
       success: function (res) {
+        console.log(cityName);
+        $("#searchHistory").prepend(`<li class="card">${res.name}</li>`);
+        var previousSearch = JSON.parse(localStorage.getItem("city")) || [];
+        previousSearch.push(res.name);
+        localStorage.setItem("city", JSON.stringify(previousSearch));
+
         console.log(res);
-        console.log(res.name);
-        console.log(res.main.temp);
 
         var kelvinTemp = parseFloat(res.main.temp);
         // console.log(kelvinTemp);
-        console.log((kelvinTemp - 273.15) * 1.8 + 32);
-
-        console.log(res.main.humidity);
+        var farenTemp = parseInt((kelvinTemp - 273.15) * 1.8 + 32);
         var weatherIcon = res.weather[0].icon;
+        var currentHumidity = res.main.humidity;
+        var cityName = res.name;
+        var wind = parseFloat(res.wind.speed * 2.237).toFixed(2);
+
         var iconUrl =
           "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
-        $("body").prepend(
-          // `<img src="https://openweathermap.org/"+'${weatherIcon}'/>`
-          `<img id="weatherIcon" src="${iconUrl}"/>`
-        );
+
+        $("#cityName").text(`${cityName} ${date}`);
+        $("#currentHumidity").text(`Humidity: ${currentHumidity}%`);
+        $("#weather-icon").attr("src", `${iconUrl}`);
+
+        $("#currentTemp").text(`${farenTemp}ºF`);
+        $("#windSpeed").text(`Wind Speed: ${wind} mph`);
+
+        var lat = res.coord.lat;
+        var lon = res.coord.lon;
+
+        $.ajax({
+          type: "GET",
+          url: `https://api.openweathermap.org/data/2.5/uvi/forecast?appid=${api_key_kenny}&lat=${lat}&lon=${lon}`,
+          dataType: "json",
+          success: function (response) {
+            console.log(response);
+            var uvIndexNum = response[0].value.toFixed(2);
+            if (uvIndexNum <= 2) {
+              $("#uvIndex").html(
+                `<div class="d-flex mr-2 align-items-baseline">UV Index:<p class="ml-2 p-1 bg-danger rounded"> ${uvIndexNum}</p> </div>`
+              );
+            } else {
+              $("#uvIndex").html(
+                `<div class="d-flex mr-2 align-items-baseline">UV Index:<p class="ml-2 p-1 bg-danger rounded"> ${uvIndexNum}</p> </div>`
+              );
+            }
+
+            // function searchHistory() {
+            //   var searchHistory =
+            //     JSON.parse(localStorage.getItem("city")) || [];
+            //   $("#searchHistory").empty();
+            //   for (i = 0; i < searchHistory.length; i++) {
+            //     $("#searchHistory").prepend(
+            //       `<li class="card">${searchHistory[i]}</li>`
+            //     );
+            //   }
+            // }
+
+            console.log(uvIndexNum);
+            $("#mainContainer").removeClass("d-none");
+          },
+        });
       },
-      // }).then(function (res) {
     });
-  });
-
-  // (function (res) {
-  //   console.log(res);
-  //   console.log(res.name);
-
-  // });
+  }
 });
